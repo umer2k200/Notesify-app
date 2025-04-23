6d9b0e3b078be5a832f84a8224c35ccdd1a08655
fadeoclock <modfadeoclock@gmail.com>
2025-04-23 14:53:48 +0500

    added vercel.json x5

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,8 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     emptyOutDir: true,
   },
-  server:{
-    historyApiFallback: true,
-  }
+  base: '/'
 })
