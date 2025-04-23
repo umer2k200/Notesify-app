908fd1afe9769dba14fd766d79d0621c31249dc6
fadeoclock <modfadeoclock@gmail.com>
2025-04-23 14:04:39 +0500

    added vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 const routes = (
   <Router>
     <Routes>
+      <Route path='/' exact element={<Login />} />
       <Route path='/dashboard' exact element={<Home />} />
       <Route path='/login' exact element={<Login/>} />
       <Route path='/signup' exact element={<SignUp/>} />
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/login',
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server:{
+    historyApiFallback: true,
+  }
 })
